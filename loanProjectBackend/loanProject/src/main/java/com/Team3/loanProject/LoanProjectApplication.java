4f17ae88382a25c37418fc9e67702301389cea72
Ravi Sadhwani <RaviSadhwani@corp.fairisaac.com>
2020-08-20 16:52:31 +0530

    Spring Starter commit

---
+++ loanProjectBackend/loanProject/src/main/java/com/Team3/loanProject/LoanProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Team3.loanProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanProjectApplication.class, args);
+	}
+
+}
---
+++ loanProjectBackend/loanProject/src/test/java/com/Team3/loanProject/LoanProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Team3.loanProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
